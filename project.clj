1707d1030a0bcd19458aecbc102cc341d218a71a
Nicolas Berger <nicoberger@gmail.com>
2015-04-24 12:10:19 -0300

    Add scm info

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://github.com/nberger/lein-tag-no-sign"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/nberger/lein-tag-no-sign.git"}
+
   :plugins [[lein-tag-no-sign "0.1.0-SNAPSHOT"]]
 
   :release-tasks [["vcs" "assert-committed"]
