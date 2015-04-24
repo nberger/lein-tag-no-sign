d9270975c44c5246107f8e081e2183b7232cb486
Nicolas Berger <nicoberger@gmail.com>
2015-04-23 23:08:53 -0300

    Use our own plugin and override release-tasks

--- project.clj
+++ project.clj
@@ -3,4 +3,16 @@
   :url "http://github.com/nberger/lein-tag-no-sign"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-tag-no-sign "0.1.0-SNAPSHOT"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["tag-no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
   :eval-in-leiningen true)
