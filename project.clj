319ae74fbbd63ae0a717b871563c52c4c35bf048
Nicolas Berger <nicoberger@gmail.com>
2015-04-24 12:49:58 -0300

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-tag-no-sign "0.1.0"
+(defproject lein-tag-no-sign "0.1.1-SNAPSHOT"
   :description "Leiningen plugin to provide a non-signing variant of vcs tag"
   :url "http://github.com/nberger/lein-tag-no-sign"
   :license {:name "Eclipse Public License"
